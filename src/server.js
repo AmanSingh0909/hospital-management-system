9e79ab38833e8eb97f58ad963fc2d3c7a0d437b6
Aman Singh <amankumarkvm4@gmail.com>
2025-12-15 22:57:39 +0530

    some changes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 const connectDB  = require('./config/db')
+const path = require('path');
+const patientRoutes = require('./routes/patientsRoutes')
 
 // dot env configration
 dotenv.config()
@@ -17,6 +19,15 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
+// Views setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => res.render('dashboard'));
+
+// Routes
+app.use('/patients', patientRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
