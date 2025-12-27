38530a55120a602ac46ffd95970df408e74e178c
Aman Singh <amankumarkvm4@gmail.com>
2025-12-27 22:32:51 +0530

    implemented authorization for user using token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,2 +1,26 @@
-const express  = require('express')
+const express = require('express')
 
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({
+                message: 'Access denied, token missing'
+            })
+        }
+
+        const token = authHeader.split(' ')[1]
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+
+        next()
+        
+    } catch (error) {
+        console.log('Auth Middleware Error:', error.message);
+        res.status(401).json({
+            message: 'Invalid or Expired token'
+        })
+    }
+}
