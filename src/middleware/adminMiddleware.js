2d3b4ce56f227e3639aceacd05085a89cac7dbd4
Aman Singh <amankumarkvm4@gmail.com>
2025-12-28 23:19:03 +0530

    updated this to work with both the auth methods

--- src/middleware/adminMiddleware.js
+++ src/middleware/adminMiddleware.js
@@ -1,17 +1,12 @@
-
-
 const adminMiddleware = (req, res, next) => {
-    try {
-        if (!req.user || req.user.role !== 'admin') {
-            return res.status(403).json({
-                message: 'Admin access required'
-            })
-        }
-    } catch (error) {
-        console.log('Admin Middleware Error:', error.message);
-        res.status(500).json({
-            message: 'Authorization Error'
-        })
-        
+  try {
+    if (!req.user || req.user.role !== 'admin') {
+      return res.status(403).json({ message: 'Admin access required' });
     }
-}
+    next();
+  } catch (error) {
+    res.status(500).json({ message: 'Authorization error' });
+  }
+};
+
+module.exports = adminMiddleware;
