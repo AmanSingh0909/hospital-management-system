d8f126fb60989d0ebd1195300f417c65f9ffc5b2
Aman Singh <amankumarkvm4@gmail.com>
2025-12-28 23:20:58 +0530

    updated auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,9 +2,11 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-
+// Render register page
+const registerPageContgroller = (req, res) => {
+    res.render('auth/register')
+}
 // Register a new user
-
 const registerUserController = async (req, res) => {
     try {
         const { name, email, password } = req.body
@@ -12,7 +14,7 @@ const registerUserController = async (req, res) => {
         // Check if user already exists
         const existingUser = await User.findOne({ email })
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' })
+            return res.render('auth/register', { error: 'User already exists' })
         }
 
         // hash password
@@ -25,32 +27,32 @@ const registerUserController = async (req, res) => {
             password: hashedPassword
         })
 
-        res.status(201).json({
-            message: 'User registered successfully'
-        })
+        res.redirect('/auth/login')
     } catch (error) {
         console.log('Register Error', error.message);
-        res.status(500).json({
-            message: 'Server error during registration'
-        })
+        res.render('auth/register', { error: 'Registration failed' })
     }
 }
 
+// render Login page
+const loginPageController = (req, res) => {
+    res.render('auth/login')
+}
 // Login User
 const LoginUserController = async (req, res) => {
     try {
         const { email, password } = req.body
         const user = await User.findOne({ email })
         if (!user) {
-            return res.status(400).json({
-                message: 'Invalid credentials'
+            return res.render('auth/login', {
+                error: 'Invalid credentials'
             })
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
-            return res.status(400).json({
-                message: 'Invalid credentials'
+            return res.render('auth/login', {
+                error: 'Invalid credentials'
             })
         }
 
@@ -59,13 +61,22 @@ const LoginUserController = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: '1d' }
         )
+
+        res.cookie('token', token, { httpOnly: true })
         res.json({ token })
     } catch (error) {
         console.log('Login Error', error.message);
-        res.status(500).json({
-            message: 'Server error during Login'
+        res.render('auth/login', {
+            error: 'Server error during Login'
         })
     }
 }
 
-module.exports = {registerUserController, LoginUserController}
+//Logout User
+const logout = (req, res) => {
+    res.clearCookie('token')
+    res.redirect('/auth/login')
+}
+
+
+module.exports = { registerPageContgroller, registerUserController, loginPageController, LoginUserController, logout }
