22e26b88e3025cc1b4c7d6db947b10c9bb3c1db9
Aman Singh <amankumarkvm4@gmail.com>
2025-12-30 22:16:42 +0530

    corrected some error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,4 @@
-const express = require(' express ')
+const express = require('express')
 const { registerUserController, LoginUserController } = require('../controllers/authController')
 const router = express.Router()
 
