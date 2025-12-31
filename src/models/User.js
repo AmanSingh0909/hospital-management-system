93e6fd979a2ce8d883c43b6ef27d43a70cb9a68f
Aman Singh <amankumarkvm4@gmail.com>
2025-12-31 22:22:47 +0530

    updated some error in user model

--- src/models/User.js
+++ src/models/User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         uniquie: true
     },
-    pasword: {
+    password: {
         type: String,
         required: true
     },
