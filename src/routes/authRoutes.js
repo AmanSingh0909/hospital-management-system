28ef5d3a639e6b5524c047d24af80ab58a533cdc
Aman Singh <amankumarkvm4@gmail.com>
2025-12-31 22:23:33 +0530

    added login and logout routes in auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { registerUserController, LoginUserController } = require('../controllers/authController')
+const { registerUserController, LoginUserController, registerPageContgroller, loginPageController, logout } = require('../controllers/authController')
 const router = express.Router()
 
+router.get('/register', registerPageContgroller)
 router.post('/register', registerUserController)
 
+router.get('/login', loginPageController)
 router.post('/login', LoginUserController)
+
+router.get('/logout', logout)
 
 module.exports = router
