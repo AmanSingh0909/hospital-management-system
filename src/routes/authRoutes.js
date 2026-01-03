7e66f7945c7697545cec9336de78f8da68252623
Aman Singh <amankumarkvm4@gmail.com>
2026-01-03 22:11:28 +0530

    added routes in authroutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { registerUserController, LoginUserController, registerPageContgroller, loginPageController, logout } = require('../controllers/authController')
+const { registerValidation, loginValidation } = require('../validators/authValidator')
+const validateRequest = require('../middleware/validateRequest')
 const router = express.Router()
 
 router.get('/register', registerPageContgroller)
-router.post('/register', registerUserController)
+router.post('/register', registerValidation, validateRequest, registerUserController)
 
 router.get('/login', loginPageController)
-router.post('/login', LoginUserController)
+router.post('/login', loginValidation, validateRequest, LoginUserController)
 
 router.get('/logout', logout)
 
