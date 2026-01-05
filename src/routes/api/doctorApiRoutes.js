62ac30371667776521ae9550d9115c4b20a373fb
Aman Singh <amankumarkvm4@gmail.com>
2026-01-05 20:58:50 +0530

    updated doctorAPI Routes

--- src/routes/api/doctorApiRoutes.js
+++ src/routes/api/doctorApiRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const authHeader = require('../../middleware/authHeaderMiddleware')
 const { getDoctor, createDoctor } = require('../../controllers/api/doctorApiController')
+const { createDoctorValidation } = require('../../validators/doctorValidator')
+const validateApiRequest = require('../../middleware/validateApiRequest')
 
 // GET /api/doctors
 router.get('/', authHeader, getDoctor)
 
-router.post('/', authHeader, createDoctor)
+router.post('/', authHeader, createDoctorValidation, validateApiRequest, createDoctor)
 
 module.exports = router
